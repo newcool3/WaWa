e257edb6db6d95952ad612fb536fcc01411c1781
Wen <newcool3@gmail.com>
2021-10-06 16:05:08 +0800

    add member register

---
+++ ReserverRideVar01/Controllers/MemberController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReserverRideVar01.DbContext;
+using ReserverRideVar01.Models;
+using ReserverRideVar01.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReserverRideVar01.Controllers
+{
+    //[Route("/[controller]/[action]")]
+    public class MemberController : Controller
+    {
+        MSITDbContext _db;
+        public MemberController(MSITDbContext db)
+        {
+            _db = db;
+        }
+        //[Route("/Member/register")]
+        public IActionResult register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult register(MemberViewModel Mem)
+        {
+            Member member = new Member();
+            member.MemberName = Mem.MemberName;
+            member.MemberNumberID = Mem.MemberNumberID;
+            member.MemberPassword = Mem.MemberPassword;
+            member.MemberPhone = Mem.MemberPhone;
+            member.MemberBirthday = Mem.MemberBirthday;
+            member.MemberModifyDate = DateTime.Now;
+            member.MemberCreateDate = DateTime.Now;
+            member.MemberEmail = Mem.MemberEmail;
+            _db.Members.Add(member);
+            _db.SaveChanges();
+            return RedirectToAction("about.html");
+        }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("List");
+            }
+            Member mem = _db.Members.FirstOrDefault(m => m.MemberID == id);
+            if (mem == null)
+            {
+                return RedirectToAction("List");
+            }
+            return View(mem);
+        }
+        [HttpPost]
+        public IActionResult Edit(Member Member)
+        {
+            Member mem = _db.Members.FirstOrDefault(m => m.MemberID == Member.MemberID);
+            if (mem != null)
+            {
+                mem.MemberName = Member.MemberName;
+                mem.MemberPassword = Member.MemberPassword;
+                mem.MemberPhone = Member.MemberPhone;
+                mem.MemberAddress = Member.MemberAddress;
+                mem.MemberBirthday = Member.MemberBirthday;
+                //mem.MemberPhoto = Member.MemberPhoto;
+                mem.MemberNumberID = Member.MemberNumberID;
+                _db.SaveChanges();
+            }
+            return RedirectToAction("List");
+        }
+
+    }
+}
---
+++ ReserverRideVar01/ViewModels/MemberViewModel.cs
@@ -0,0 +1,79 @@
+﻿using ReserverRideVar01.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReserverRideVar01.ViewModel
+{
+    public class MemberViewModel
+    {
+        public Member _member { get; set; }
+        public MemberViewModel ()
+        {
+            this._member = new Member();
+        }
+        [Key]//pk
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]//識別規格
+        public int MemberID { get; set; } //int 型別
+
+        [Required]//不能空值
+        [MaxLength(50)]//最大長度
+        [Column(TypeName = "nvarchar")]
+        public string MemberName
+        {
+            get { return this._member.MemberName; }
+            set { this._member.MemberName = value; }
+        }
+
+        [Required]//不能空值
+        [MaxLength(50)]//最大長度
+        [Column(TypeName = "varchar")]
+        public string MemberPhone
+        {
+            get { return this._member.MemberPhone; }
+            set { this._member.MemberPhone = value; }
+        }
+
+        [Required]//不能空值
+        [MaxLength(50)]//最大長度
+        [Column(TypeName = "nvarchar")]
+        public string MemberPassword
+        {
+            get { return this._member.MemberPassword; }
+            set { this._member.MemberPassword = value; }
+        }
+
+        [Required]//不能空值
+        [Column(TypeName = "datetime")]
+        public DateTime MemberBirthday 
+        {
+            get { return this._member.MemberBirthday; }
+            set {this._member.MemberBirthday=value; } 
+        }
+
+        [Required]//不能空值
+        [MaxLength(10)]//最大長度
+        [Column(TypeName = "varchar")]
+        public string MemberNumberID 
+        { 
+            get { return this._member.MemberNumberID; } 
+            set { this._member.MemberNumberID = value; } 
+        }
+
+        [MaxLength(50)]//最大長度
+        [Column(TypeName = "nvarchar")]
+        public string MemberAddress { get; set; }
+
+        [Required]//不能空值
+        [MaxLength(50)]//最大長度
+        [Column(TypeName = "nvarchar")]
+        public string MemberEmail
+        {
+            get { return this._member.MemberEmail; }
+            set { this._member.MemberEmail = value; }
+        }
+    }
+}
