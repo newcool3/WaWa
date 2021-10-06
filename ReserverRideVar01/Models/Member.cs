6ea9a27b58d39f4d7872ab5c7e1c4097f4e4ddb1
Wen <newcool3@gmail.com>
2021-10-06 15:37:31 +0800

    add user login function

--- ReserverRideVar01/Models/Member.cs
+++ ReserverRideVar01/Models/Member.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 #nullable disable
 
@@ -7,11 +9,58 @@ namespace ReserverRideVar01.Models
 {
     public partial class Member
     {
-        public int MemberId { get; set; }
+        [Key]//pk
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]//識別規格
+        public int MemberID { get; set; } //int 型別
+
+        [Required]//不能空值
+        [MaxLength(50)]//最大長度
+        [Column(TypeName = "nvarchar")]
         public string MemberName { get; set; }
+
+        [Required]//不能空值
+        [MaxLength(50)]//最大長度
+        [Column(TypeName = "varchar")]
         public string MemberPhone { get; set; }
-        public string MemberEmail { get; set; }
-        public string MemberAddress { get; set; }
+
+        [Required]//不能空值
+        [MaxLength(50)]//最大長度
+        [Column(TypeName = "nvarchar")]
         public string MemberPassword { get; set; }
+
+
+        [MaxLength(50)]//最大長度
+        [Column(TypeName = "nvarchar")]
+        public string MemberAddress { get; set; }
+
+        [Required]//不能空值
+        [Column(TypeName = "datetime")]
+        public DateTime MemberBirthday { get; set; }
+
+        [Required]//不能空值
+        [MaxLength(10)]//最大長度
+        [Column(TypeName = "varchar")]
+        public string MemberNumberID { get; set; }
+
+        [MaxLength(10)]//最大長度
+        [Column(TypeName = "varchar")]
+        public string MemberEnable { get; set; }
+
+        [MaxLength]//最大長度
+        [Column(TypeName = "varbinary")]
+        public byte[] MemberPhoto { get; set; }
+
+        public int MemberType { get; set; } //int 型別
+
+        [Column(TypeName = "datetime")]
+        public DateTime MemberCreateDate { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime MemberModifyDate { get; set; }
+
+        [Required]//不能空值
+        [MaxLength(50)]//最大長度
+        [Column(TypeName = "nvarchar")]
+        public string MemberEmail { get; set; }
     }
 }
