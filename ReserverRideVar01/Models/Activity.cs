b67b2794e35c0aad8dd4c2feb51d8d218e965b04
Fang-Ru,Fu <a4641146@gmail.com>
2021-10-06 10:20:35 +0800

    Add Activity Table

---
+++ ReserverRideVar01/Models/Activity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace test02.Models
+{
+    public partial class Activity
+    {
+        public int ActivityId { get; set; }
+        public string ActivityName { get; set; }
+        public string ActivityType { get; set; }
+        public string ActivityTimezone { get; set; }
+        public DateTime ActivityStartDate { get; set; }
+        public DateTime ActivityEndDate { get; set; }
+        public int ActivityPrice { get; set; }
+        public string ActivityLocation { get; set; }
+        public string ActivityState { get; set; }
+        public int ActivityNumberLimit { get; set; }
+        public DateTime ActivityDeadline { get; set; }
+    }
+}
