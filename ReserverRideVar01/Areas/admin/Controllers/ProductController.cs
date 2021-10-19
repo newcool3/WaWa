9b72ea39bbda92ded212e68887e59af4b58d79b7
David <David@DESKTOP-CB21JV8>
2021-10-19 18:02:23 +0800

    add invoice at admin panel

--- ReserverRideVar01/Areas/admin/Controllers/ProductController.cs
+++ ReserverRideVar01/Areas/admin/Controllers/ProductController.cs
@@ -11,14 +11,14 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace ReserverRideVar01.Areas.admin.Controllers
 {
     [Area("admin")]
     public class ProductController : Controller
     {
-        string hashKey = "OLkdjTkzOoNYjDmeFmCk7CORlRTo4F47";
-        string hashIV = "CHsEQUu2AbRqDtQP";
         MSITDbContext _db;
 
         public ProductController(MSITDbContext db)
@@ -52,7 +52,7 @@ namespace ReserverRideVar01.Areas.admin.Controllers
             var TradeInfo = Request.Form["TradeInfo"];
             var TradeSha = Request.Form["TradeSha"];
             string aes = DecryptAES256(TradeInfo);
-            
+
             JObject json = JObject.Parse(aes);
             ViewBag.MerchantOrderNo = json["Result"]["MerchantOrderNo"];
             ViewBag.PayTime = json["Result"]["PayTime"];
@@ -62,6 +62,43 @@ namespace ReserverRideVar01.Areas.admin.Controllers
 
             return View();
         }
+
+        public static void SendMailByGmail()
+        {
+            // 建立郵件
+            var message = new MimeMessage();
+
+            // 添加寄件者
+            message.From.Add(new MailboxAddress("寄件者名稱", "a0975463529@gmail.com"));
+
+            // 添加收件者
+            message.To.Add(new MailboxAddress("收件者名稱", "jianjunwei0406@gmail.com"));
+
+            // 設定郵件標題
+            message.Subject = "發票";
+
+            // 使用 BodyBuilder 建立郵件內容
+            var bodyBuilder = new BodyBuilder();
+
+            // 設定文字內容
+            bodyBuilder.TextBody = "文字內容";
+
+            // 設定 HTML 內容
+            bodyBuilder.HtmlBody = "<p> HTML 內容 </p>";
+
+            // 設定附件
+            //bodyBuilder.Attachments.Add("檔案路徑");
+
+            // 設定郵件內容
+            message.Body = bodyBuilder.ToMessageBody();
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, true);
+            client.Authenticate("a0975463529@gmail.com", "0975463529");
+            client.Send(message);
+            client.Disconnect(true);
+            client.Dispose();
+        }
+
         public IActionResult Index()
         {
             ProductViewModel productView = new ProductViewModel();
@@ -150,8 +187,8 @@ namespace ReserverRideVar01.Areas.admin.Controllers
 
         public string DecryptAES256(string encryptData)//解密
         {
-            string sSecretKey = hashKey;
-            string iv = hashIV;
+            string sSecretKey = Dictionary.hashKey;
+            string iv = Dictionary.hashIV;
             var encryptBytes = HexStringToByteArray(encryptData.ToUpper());
             var aes = new RijndaelManaged();
             aes.Key = Encoding.UTF8.GetBytes(sSecretKey);
