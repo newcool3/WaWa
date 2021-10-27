ca19493bc8e17fbae2de1f199e5901d220960d6c
Fang-Ru,Fu <a4641146@gmail.com>
2021-10-27 14:21:20 +0800

    add-ViewModel-Activity/shoppingCart/addtoCart

---
+++ ReserverRideVar01/ViewModels/ActivityViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using ReserverRideVar01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReserverRideVar01.ViewModels
+{
+    public class ActivityViewModel
+    {
+        public Models.Activity activity { get; set; }
+
+        public ActivityViewModel()
+        {
+            this.activity = new Activity();
+        }
+
+        public int ActivityId { get; set; }
+
+        public string ActivityName { get; set; }
+
+        public string ActivityType { get; set; }
+
+        public DateTime ActivityStartDate { get; set; }
+
+        public DateTime ActivityEndDate { get; set; }
+
+        public string ActivityTimezone { get; set; }
+
+        public int ActivityPrice { get; set; }
+
+        public string ActivityLocation { get; set; }
+
+        public string ActivityState { get; set; }
+
+        public int ActivityNumberLimit { get; set; }
+        public string ActivityDescription { get; set; }
+        public DateTime ActivityDeadline { get; set; }
+        public IFormFile ActivityPhoto { get; set; }
+        public int IslandId { get; set; }
+
+        public virtual Island Island { get; set; }
+
+    }
+}
+
